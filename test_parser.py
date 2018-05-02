704b0db8399b7bab46a06ea6bb1eb9e4a759e744
Rachel Hirshman <rdhirsh@stanford.edu>
2018-05-01 19:26:16 -0700

    Added more functionality, still not perfect and memory errors when trying to parse all documents at once

--- test_parser.py
+++ test_parser.py
@@ -3,6 +3,8 @@
 
 from bs4 import BeautifulSoup
 import unicodedata
+import os
+import csv
 
 def remove_diacritic(input):
     '''
@@ -27,14 +29,84 @@ def parseFile(file):
         value = talk.find('p').get_text()
         value = remove_diacritic(value).decode('utf-8')
 
+        speech = talk.find_all('p')
+        text = ""
+        for section in speech:
+            text = text + section.get_text()
+        value = remove_diacritic(text).decode('utf-8')
+
+        if key and value:
+            # Check if the key already exists
+            if speaker_text_dict.has_key(key):
+                current_value = speaker_text_dict[key]
+                new_value = current_value + value
+                speaker_text_dict[key] = new_value
+            else:
+                speaker_text_dict[key] = value
+
         speaker_text_dict[key] = value
 
-    #print(speaker_text_dict)
-    print(speaker_text_dict.keys())
+    # Writes the dictionary to a csv file to sanity check. Still some parsing errors
+    with open('Speaker_file.csv', 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        for key, value in speaker_text_dict.items():
+            writer.writerow([key, value])
 
+    file.close()
+
+    #print(speaker_text_dict.keys())
+
+
+# Code for parsing all files but getting memory errors
+"""def parseFiles():
+    #file = open(sys.argv[1], "r")
+    speaker_text_dict = {}
+    for filename in os.listdir("Docs/"):
+        if filename.endswith(".xml"):
+            filename = open('Docs/' + filename, "r")
+            contents = filename.read()
+            soup = BeautifulSoup(contents, 'lxml')
+            for talk in soup.find_all('sp'):
+                # Key is speaker name
+                if talk.find('speaker'):
+                    key = talk.find('speaker').get_text()
+                #key = talk.find('speaker').get_text()
+                    key = remove_diacritic(key).decode('utf-8')
+                
+                # Value is speaker speech
+
+                # Code for only one paragraph, mostly used for testing
+                value = talk.find('p').get_text()
+                value = remove_diacritic(value).decode('utf-8')
+
+                # Code for when there are multiple paragraphs
+                speech = talk.find_all('p')
+                text = ""
+                for section in speech:
+                    text = text + section.get_text()
+                value = remove_diacritic(text).decode('utf-8')
+
+                # Make sure that key and value exist
+                if key and value:
+                    # Check if the key already exists
+                    if speaker_text_dict.has_key(key):
+                        current_value = speaker_text_dict[key]
+                        new_value = current_value + value
+                        speaker_text_dict[key] = new_value
+                    else:
+                        speaker_text_dict[key] = value
+            filename.close()
+        else:
+            continue
+    print(speaker_text_dict['M. Boutteville- Dumetz.'])
+    #print(speaker_text_dict.keys())"""
 
 
 if __name__ == '__main__':
     import sys
+    
+    
+    #parseFiles()
+    
     file_name = sys.argv[1]
     parseFile(file_name)
