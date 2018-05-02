490230645c3e29a837a5807a52d3e0df9085c07f
Rachel Hirshman <rdhirsh@stanford.edu>
2018-05-02 16:07:56 -0700

    added code to remove stopwords

--- test_ngrams.py
+++ test_ngrams.py
@@ -5,8 +5,10 @@ import nltk
 import collections
 from nltk import word_tokenize
 from nltk.util import ngrams
+from nltk.corpus import stopwords
 from collections import Counter
 import pickle
+import unicodedata
 
 def compute_bigrams(input):
     token = word_tokenize(input)
@@ -23,6 +25,25 @@ def compute_fourgrams(input):
     fourgrams = ngrams(token, 4)
     return fourgrams
 
+def remove_diacritic(input):
+    '''
+    Accept a unicode string, and return a normal string (bytes in Python 3)
+    without any diacritical marks.
+    '''
+    return unicodedata.normalize('NFKD', input).encode('ASCII', 'ignore')
+
+def remove_stopwords(input):
+    french_stopwords = stopwords.words('french')
+    cleaned_stopwords = []
+    cleaned_stopwords.append("les")
+    for stopword in french_stopwords:
+        cleaned_stopwords.append(remove_diacritic(stopword))
+    filtered_text = ""
+    for word in input.split():
+        if word not in french_stopwords:
+            if word != "les" and word != "a":
+                filtered_text = filtered_text + " " + word
+    return filtered_text
 
 if __name__ == '__main__':
     import sys
@@ -32,32 +53,29 @@ if __name__ == '__main__':
     with open(filename, 'rb') as handle:
         dictionary = pickle.load(handle)
 
-    text = dictionary['M. Paul Nairac']
-    text = text.replace(",", "").replace(".", "")
+    """text = ""
+    for entry in dictionary:
+        text = text + dictionary[entry]
+    text = text.replace(",", "").replace(".", "").replace(":","")
+    clean_text = remove_stopwords(text)"""
 
-    new_bigrams = compute_bigrams(text)
+    text = dictionary['M. Paul Nairac'].replace(";","").replace(",", "").replace(":","").replace(".","").replace("(","").replace(")","")
+    clean_text = remove_stopwords(text)
+
+    new_bigrams = compute_bigrams(clean_text)
     bigram_count = Counter(new_bigrams)
     print("BIGRAMS")
     print(bigram_count.most_common(10))
     print("\n")
 
-    new_trigrams = compute_trigrams(text)
+    new_trigrams = compute_trigrams(clean_text)
     trigram_count = Counter(new_trigrams)
     print("TRIGRAMS")
     print(trigram_count.most_common(10))
     print("\n")
 
-    new_fourgrams = compute_fourgrams(text)
+    new_fourgrams = compute_fourgrams(clean_text)
     fourgram_count = Counter(new_fourgrams)
     print("FOURGRAMS")
     print(fourgram_count.most_common(10))
     print("\n")
-
-    with open('Nairac_bigram.pickle', 'wb') as handle:
-        pickle.dump(bigram_count, handle, protocol = 0)
-
-    with open('Nairac_trigram.pickle', 'wb') as handle:
-        pickle.dump(trigram_count, handle, protocol = 0)
-
-    with open('Nairac_fourgram.pickle', 'wb') as handle:
-        pickle.dump(fourgram_count, handle, protocol = 0)
