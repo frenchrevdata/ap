e9da064ef5c628bfe99d47307b4dcd5679672245
Rachel Hirshman <rdhirsh@stanford.edu>
2018-05-28 17:13:00 -0700

    code outputs csv files aggregating each group

--- Girondins_Montagnards.py
+++ Girondins_Montagnards.py
@@ -22,6 +22,8 @@ speechid_to_speaker = {}
 speaker_names = set()
 
 global speakers_to_analyze
+Girondins = Counter()
+Montagnards = Counter()
 
 
 def remove_diacritic(input):
@@ -50,8 +52,35 @@ def aggregate_by_speaker():
 def aggregate_by_group():
 	files = os.listdir("../Speakers/")
 	for filename in files:
-		speaker = re.findall(r'([a-zA-Z\- \']+)_ngrams.pickle', filename)
-		print(speaker[0])
+		with open('../Speakers/' + filename, "r") as f:
+			speaker_data = pickle.load(f)
+		#speaker_data = pickle.load(open('../Speakers/' + filename, "r"))
+		speaker = re.findall(r'([a-zA-Z\- \']+)_ngrams.pickle', filename)[0]
+		party = speakers_to_analyze.loc[speaker, "Party"]
+		if party == "Girondins":
+			try:
+				Girondins = Girondins + speaker_data
+			except NameError:
+				Girondins = speaker_data
+		else:
+			try:
+				Montagnards = Montagnards + speaker_data
+			except NameError:
+				Montagnards = speaker_data
+
+	Gir_output_file = "Girondins_counts.csv"
+	with open(Gir_output_file, mode='w') as gf:
+		gf.write('Bigrams|freq\n')
+		for bigram, count in Girondins.items():
+			if count >= 3:
+				gf.write('{}|{}\n'.format(bigram, count))
+	Mont_output_file = "Montagnards_counts.csv"
+	with open(Mont_output_file, mode='w') as mf:
+		mf.write('Bigrams|freq\n')
+		for bigram, count in Montagnards.items():
+			if count >= 3:
+				mf.write('{}|{}\n'.format(bigram, count))
+		
 
 
 def load_list(speakernames):
