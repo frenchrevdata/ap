839cd11a63bf05abe050f731a9f68acca775ef17
Rachel Hirshman <rdhirsh@stanford.edu>
2018-07-09 22:30:28 -0700

    includes topic modeling

--- marat_vote.py
+++ marat_vote.py
@@ -18,6 +18,10 @@ import os
 import gzip
 from make_ngrams import compute_ngrams
 import xlsxwriter
+import string
+from nltk.stem.wordnet import WordNetLemmatizer
+import gensim
+from gensim import corpora
 
 
 #global speaker_list
@@ -32,33 +36,79 @@ def remove_diacritic(input):
     return unicodedata.normalize('NFKD', input).encode('ASCII', 'ignore')
 
 
-def parseFile(justifications):
-    file = open('marat.xml', "r")
-    contents = file.read()
-    contents = re.sub(r'(<p>(?:DÉPARTEMENT|DEPARTEMENT|DÉPARTEMENE)[\s\S]{1,35}<\/p>)', '', contents)
-    soup = BeautifulSoup(contents, 'lxml')
-    for talk in soup.find_all('sp'):
-    	speaker = talk.find('speaker').get_text()
-    	speaker = remove_diacritic(speaker).decode('utf-8')
-    	speaker = speaker.replace(".","")
+def parseFile(votes):
+	justifications = []
+	file = open('marat.xml', "r")
+	contents = file.read()
+	contents = re.sub(r'(<p>(?:DÉPARTEMENT|DEPARTEMENT|DÉPARTEMENE)[\s\S]{1,35}<\/p>)', '', contents)
+	soup = BeautifulSoup(contents, 'lxml')
+	for talk in soup.find_all('sp'):
+		speaker = talk.find('speaker').get_text()
+		speaker = remove_diacritic(speaker).decode('utf-8')
+		speaker = speaker.replace(".","")
 
-    	speech = talk.find_all('p')
-    	text = ""
-    	full_speech = ""
-    	for section in speech:
-    		text = text + section.get_text()
+		speech = talk.find_all('p')
+		text = ""
+		full_speech = ""
+		for section in speech:
+			text = text + section.get_text()
 		full_speech = remove_diacritic(text).decode('utf-8')
 		full_speech = full_speech.replace('\n', '').replace('\t', '').replace('\r','')
 		full_speech = re.sub(r'([ ]{2,})', ' ', full_speech)
 
-		justifications[speaker_name] = full_speech
+		if len(full_speech) > 30:
+			justifications.append(full_speech)
 
-	df = pd.DataFrame.from_dict(justifications, orient = 'index')
-    writer = pd.ExcelWriter('Marat_Justifications.xlsx')
-    df.to_excel(writer)
-    writer.save()
-    file.close()
+		votes[speaker] = full_speech
 
+	runTopicModel(justifications)
+
+	df = pd.DataFrame.from_dict(votes, orient = 'index')
+	writer = pd.ExcelWriter('Marat_Justifications.xlsx')
+	df.to_excel(writer)
+	writer.save()
+	file.close()
+
+def remove_stopwords(input, stopwords):
+	filtered_text = ""
+	for word in input.split():
+		if word not in stopwords:
+			filtered_text = filtered_text + " " + word
+	return filtered_text
+
+def clean(just_speech):
+	stopwords_from_file = open('FrenchStopwords.txt', 'r')
+	lines = stopwords_from_file.readlines()
+	french_stopwords = []
+	for line in lines:
+		word = line.split(',')
+		#remove returns and new lines at the end of stop words so the parser catches matches
+		#also remove accents so the entire analysis is done without accents
+		word_to_append = remove_diacritic(unicode(word[0].replace("\n","").replace("\r",""), 'utf-8'))
+		french_stopwords.append(word_to_append)
+
+	just_speech = just_speech.replace("]"," ").replace("[", " ").replace("&"," ").replace(">"," ").replace("#"," ").replace("/"," ").replace("\`"," ").replace("'"," ").replace("*", " ").replace("`", " ").replace(";"," ").replace("?"," ").replace(",", " ").replace(":"," ").replace("."," ").replace("("," ").replace(")"," ")
+	clean_text = remove_stopwords(just_speech.lower(), french_stopwords)
+	clean_text = clean_text.replace("marat", " ").replace("accusation"," ")
+	#exclude = set(string.punctuation)
+	#no_punc = ''.join(ch for ch in clean_text if ch not in exclude)
+	return clean_text
+
+def runTopicModel(justifications):
+	clean_speeches = [clean(justification).split() for justification in justifications]
+	dictionary = corpora.Dictionary(clean_speeches)
+	doc_term_matrix = [dictionary.doc2bow(doc) for doc in clean_speeches]
+	Lda = gensim.models.ldamodel.LdaModel
+	number_of_topics = 5
+	number_of_words = 3
+	ldamodel = Lda(doc_term_matrix, num_topics = number_of_topics, id2word = dictionary, passes = 50)
+	results = ldamodel.print_topics(num_topics = number_of_topics, num_words = number_of_words)
+	filename = "topic_modeling_" + str(number_of_topics) + "_numtopics_" + str(number_of_words) + "_numwords.txt"
+	file = open(filename, 'w')
+	for topic in results:
+		file.write(topic[1] + "\n")
+	file.close()
+	print(results)
 
 """def load_speakerlist(speakernames):
 	pd_list = pd.read_excel(speakernames, sheet_name= 'AP Speaker Authority List xlsx')
@@ -74,8 +124,8 @@ def parseFile(justifications):
 if __name__ == '__main__':
     import sys
     #speaker_list = load_speakerlist('Copy of AP_Speaker_Authority_List_Edited_2.xlsx')
-    justifications = {}
-    parseFile(justifications)
+    votes = {}
+    parseFile(votes)
     """df = pd.DataFrame.from_dict(justifications)
     writer = pd.ExcelWriter('Marat_Justifications.xlsx')
     df.to_excel(writer)
